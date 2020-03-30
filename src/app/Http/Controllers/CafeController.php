71d40ea761ea7aecb047003b49281038045c6786
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-03-30 22:32:21 +0900

    ログインしていない人は見れないようにしました。

--- src/app/Http/Controllers/CafeController.php
+++ src/app/Http/Controllers/CafeController.php
@@ -7,6 +7,11 @@ use App\Models\Cafe;
 
 class CafeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $cafes = Cafe::all();
