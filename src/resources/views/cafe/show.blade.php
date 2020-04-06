bc2e4ab750e991b45c35fc510c92124a8f497535
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-04-06 23:07:45 +0900

    フォーマットを修正しました。

--- src/resources/views/cafe/show.blade.php
+++ src/resources/views/cafe/show.blade.php
@@ -4,31 +4,31 @@
 @section('content')
 <div class="card">
     <div class="card-header">
-        {{$cafe->name}}
+        {{ $cafe->name }}
     </div>
     <div class="card-body">
         <div class="row">
             <div class="col">
                 <h4>カフェの場所</h4>
-                <p>{{$cafe->place}}</p>
+                <p>{{ $cafe->place }}</p>
             </div>
         </div>
         <div class="row">
             <div class="col">
                 <h4>料理の評価</h4>
-                <p>{{$cafe->food_evaluation}}</p>
+                <p>{{ $cafe->food_evaluation }}</p>
             </div>
         </div>
         <div class="row">
             <div class="col">
                 <h4>アクセスの評価</h4>
-                <p>{{$cafe->access_evaluation}}</p>
+                <p>{{ $cafe->access_evaluation }}</p>
             </div>
         </div>
         <div class="row">
             <div class="col">
                 <h4>雰囲気の評価</h4>
-                <p>{{$cafe->feeling_evaluation}}</p>
+                <p>{{ $cafe->feeling_evaluation }}</p>
             </div>
         </div>
     </div>
