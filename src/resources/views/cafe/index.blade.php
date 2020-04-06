f0d3836c09010cfb0eb0d403b300898ebd46ad18
suguki <suguki.cr@gmail.com>
2020-04-06 23:03:52 +0900

    index部分を作成。 編集画面で編集した名前、アクセス、評価項目をindexに反映できるようにした。

--- src/resources/views/cafe/index.blade.php
+++ src/resources/views/cafe/index.blade.php
@@ -2,17 +2,24 @@
 
 @section('content')
 
-<a href="{{route('cafe.create')}}" class="btn btn-primary">カフェを新規作成</a>
+<a href="{{ route('cafe.create' )}}" class="btn btn-primary">カフェを新規作成</a>
 
 @forelse($cafes as $index => $cafe)
 <div class="card">
     <div class="card-header">
-        <a href="{{route('cafe.edit', ['id' => $cafe->id])}}">
-            {{ $cafe->id }}
+        <a href="{{ route('cafe.edit', ['id' => $cafe->id]) }}">
+            {{ $cafe->name }}を編集する
         </a>
     </div>
     <div class="card-body">
         <p>{{ $cafe->name }}</p>
+        <p>{{ $cafe->place }}</p>
+        <p>料理</p>
+        <p>{{ $cafe->food_evaluation }}</p>
+        <p>アクセス</p>
+        <p>{{ $cafe->access_evaluation }}</p>
+        <p>雰囲気</p>
+        <p>{{ $cafe->feeling_evaluation }}</p>
         <p>{{ $cafe->created_at }}</p>
         <p>{{ $cafe->updated_at }}</p>
     </div>
