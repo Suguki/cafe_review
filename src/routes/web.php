2d6b1e493d6b704bc7427990700b9926d20a46cc
Nozomi Hosaka <hosaka.non.work@gmail.com>
2020-05-12 22:42:22 +0900

    ログインしていないときは閲覧のみできるようにしました。

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,25 +15,23 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'TopController@index');
+Route::get('/', 'CafeController@index')->name('cafe.index');
 
-Route::get('/cafe', 'CafeController@index')->name('cafe.index');
+Route::get('/home', 'CafeController@index')->name('home');
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/cafe/create', 'CafeController@create')->name('cafe.create');
+Route::get('/cafe/create', 'CafeController@create')->middleware('auth')->name('cafe.create');
 
 Route::get('/cafe/{id}', 'CafeController@show')->name('cafe.show');
 
-Route::post('/cafe/store', 'CafeController@store')->name('cafe.store');
+Route::post('/cafe/store', 'CafeController@store')->middleware('auth')->name('cafe.store');
 
-Route::get('/cafe/edit/{id}', 'CafeController@edit')->name('cafe.edit');
+Route::get('/cafe/edit/{id}', 'CafeController@edit')->middleware('auth')->name('cafe.edit');
 
-Route::post('/cafe/update/{id}', 'CafeController@update')->name('cafe.update');
+Route::post('/cafe/update/{id}', 'CafeController@update')->middleware('auth')->name('cafe.update');
 
-Route::post('/cafe/delete/{id}', 'CafeController@delete')->name('cafe.delete');
+Route::post('/cafe/delete/{id}', 'CafeController@delete')->middleware('auth')->name('cafe.delete');
 
-Route::post('/cafe/upload/{id}', 'CafeController@upload')->name('cafe.upload');
+Route::post('/cafe/upload/{id}', 'CafeController@upload')->middleware('auth')->name('cafe.upload');
 
 Route::get('/cafe/{cafe_id}/review/create', 'ReviewController@create')->name('review.create');
 
