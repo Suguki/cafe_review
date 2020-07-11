91e43b344f20f7941cc64f459cc2c9b3d66a3ba1
suguki <suguki.cr@gmail.com>
2020-07-11 19:01:30 +0900

    Topのレイアウトを変更。

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <a href="{{route('cafe.index')}}">リンク</a>
-</div>
 
+<head>
+  <link href="css/Top.css" rel="stylesheet" type="text/css" />
+</head>
+
+  <div class="wrapper">
+
+    <div class="box">
+      <h1 class="logo">
+        <div class="logo-title">Trip-Coffee<span class="period">.</span></div>
+      </h1>
+      <div class="container">
+    <a href="{{route('cafe.index')}}" class="btn">コンテンツ一覧へ</a>
+    </div>
+    </div>
+  </div>
+  <script src="js/script.js"></script>
 @endsection
